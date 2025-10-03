4302bd7440a87a96ee5641ac242be8f3a9fa750b
marino5710 <brianmarin4@gmail.com>
2025-10-03 08:56:20 -0600

    change

--- database/seeders/UsuarioAdminSeeder.php
+++ database/seeders/UsuarioAdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UsuarioAdminSeeder extends Seeder
 {
@@ -18,16 +19,31 @@ class UsuarioAdminSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        // Insertar usuario admin
+        // Insertar primer usuario admin
         DB::table('users')->insert([
-            'name' => 'Administrador',
-            'email' => 'admin@develotech.com',
-            'password' => Hash::make('admin123'), // Hashea la password
-            'rol_id' => $rolId,
-            'email_verified_at' => now(),
-            'remember_token' => \Str::random(10),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'user_primer_nombre' => 'Administrador',
+            'user_primer_apellido' => 'Sistema',
+            'email' => 'develotech@outlook.es',
+            'password' => Hash::make('admin123'),
+            'user_rol' => $rolId,
+            'user_fecha_verificacion' => now(),
+            'remember_token' => Str::random(10),
+            'user_situacion' => 1,
+            'user_fecha_creacion' => now(),
+        ]);
+
+        // Insertar segundo usuario admin
+        DB::table('users')->insert([
+            'user_primer_nombre' => 'BRIAN',
+            'user_segundo_nombre' => 'DANIEL',
+            'user_primer_apellido' => 'MARIN',
+            'email' => 'brianmarin4@gmail.com',
+            'password' => Hash::make('admin123'),
+            'user_rol' => $rolId,
+            'user_fecha_verificacion' => now(),
+            'remember_token' => Str::random(10),
+            'user_situacion' => 1,
+            'user_fecha_creacion' => now(),
         ]);
     }
 }
