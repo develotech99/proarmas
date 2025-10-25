6cade0672c8503419cd9f2db8c5273e652eb6f2a
brian <brianmarin4@gmail.com>
2025-10-25 22:52:03 +0000

    corrección

--- resources/views/inventario/index.blade.php
+++ resources/views/inventario/index.blade.php
@@ -33,7 +33,7 @@
             <button onclick="inventarioManager.openRegistroModal()"
                 class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                 <i class="fas fa-plus mr-2"></i>
-                Registrar Producto
+                Crear Producto
             </button>
             <button onclick="inventarioManager.openIngresoModal()"
                 class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
